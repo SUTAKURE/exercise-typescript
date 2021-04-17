5a7229e57e5f924cd1cccca47c2b38e53188088a
SUTAKURE <udonnamerou@gmail.com>
2021-04-17 12:01:36 +0900

    typescriptの環境構築

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ module.exports = {
     bundle: "./src/index.ts",
   },
   output: {
-    path: path.jsoin(__dirname, "dist"),
+    path: path.join(__dirname, "dist"),
     filename: "[name].js",
   },
   resolve: {
