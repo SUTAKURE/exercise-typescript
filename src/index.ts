5c7d1cdf574789449fdbaab4ba374485f51c6028
SUTAKURE <udonnamerou@gmail.com>
2021-04-20 23:17:54 +0900

    関数とパラメーターと戻り値に型を付ける

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,27 @@
 // world.sayHello(root);
 
 // 基本の型定義
-import {
-  anySample,
-  notExistSample,
-  primitiveSample,
-  unknownSample,
-} from "./basic";
+// import {
+//   anySample,
+//   notExistSample,
+//   primitiveSample,
+//   unknownSample,
+// } from "./basic";
 
-primitiveSample();
-notExistSample();
-anySample();
-unknownSample();
+// primitiveSample();
+// notExistSample();
+// anySample();
+// unknownSample();
+
+// 関数の型定義
+import { logMassage } from "./function/basic";
+import {
+  isUserSignedIn,
+  isUserSignedIn2,
+  sumProductsPrice,
+} from "./function/parameters";
+logMassage("Hello Typescript!");
+isUserSignedIn("ABC", "PMBOOK");
+isUserSignedIn("DEF");
+isUserSignedIn2("ABC");
+console.log(sumProductsPrice(100, 200, 300, 400, 500));
